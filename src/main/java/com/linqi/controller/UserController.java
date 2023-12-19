aa7dfb417776ebca0fa0914075107803ff6b4b44
linqi <wkw4399xs@gmail.com>
2023-12-19 22:05:18 +0800

    feat:用户退出登录功能实现

--- src/main/java/com/linqi/controller/UserController.java
+++ src/main/java/com/linqi/controller/UserController.java
@@ -57,8 +57,9 @@ public class UserController {
      */
     @PostMapping("/logout")
     public Result logout(){
-        // TODO 实现登出功能
-        return Result.fail("功能未完成");
+        UserHolder.removeUser();
+        return Result.ok();
+
     }
 
     @GetMapping("/me")
