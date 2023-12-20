3d012f6a0fd875594e84e1dd8aadab4baeed11df
linqi <wkw4399xs@gmail.com>
2023-12-20 20:49:28 +0800

    feat: Redis 缓存空值解决缓存穿透问题

--- src/main/java/com/linqi/service/impl/ShopServiceImpl.java
+++ src/main/java/com/linqi/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.linqi.constants.RedisConstants.CACHE_SHOP_KEY;
-import static com.linqi.constants.RedisConstants.CACHE_SHOP_TTL;
+import static com.linqi.constants.RedisConstants.*;
 
 /**
  * <p>
@@ -46,11 +45,17 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.ok(shop);
         }
 
+        // 判断命中的是否是空值
+        if(cache != null){
+            return Result.fail("店铺信息不存在！");
+        }
+
         // 4. 不存在，查询数据库
         Shop shop = getById(id);
 
         // 5. 数据库中依旧不存在，直接返回错误
         if(shop == null){
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("店铺不存在，抱歉!");
         }
 
