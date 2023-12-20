df83dbfa76de2e4db9f9060a99f85f0bb3676579
linqi <wkw4399xs@gmail.com>
2023-12-20 17:04:42 +0800

    feat: Redis 商户信息缓存查询接口修改

--- src/main/java/com/linqi/controller/ShopController.java
+++ src/main/java/com/linqi/controller/ShopController.java
@@ -32,7 +32,7 @@ public class ShopController {
      */
     @GetMapping("/{id}")
     public Result queryShopById(@PathVariable("id") Long id) {
-        return Result.ok(shopService.getById(id));
+        return shopService.queryById(id);
     }
 
     /**
