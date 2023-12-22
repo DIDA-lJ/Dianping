84b4833cbe0a62e5b2d3e13950eb50e5e764b5d7
linqi <wkw4399xs@gmail.com>
2023-12-22 16:01:52 +0800

    feat: Redis 缓存工具类实现

--- src/test/java/com/linqi/HmDianPingApplicationTests.java
+++ src/test/java/com/linqi/HmDianPingApplicationTests.java
@@ -1,9 +1,19 @@
 package com.linqi;
 
+import com.linqi.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private ShopServiceImpl shopService;
 
+    @Test
+    void testSaveShop(){
+        shopService.saveShop2Redis(1L,10L);
+    }
 
 }
