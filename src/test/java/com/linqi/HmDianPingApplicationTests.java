17491abb2e5ac836c330b77b6aba53f019189ec2
linqi <wkw4399xs@gmail.com>
2023-12-23 15:21:03 +0800

    feat: Redis 全局 ID 生成器具

--- src/test/java/com/linqi/HmDianPingApplicationTests.java
+++ src/test/java/com/linqi/HmDianPingApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -16,4 +18,10 @@ class HmDianPingApplicationTests {
         shopService.saveShop2Redis(1L,10L);
     }
 
+    @Test
+    void testStartTime(){
+        LocalDateTime time = LocalDateTime.of(2023,12,13,0,0,0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+    }
 }
