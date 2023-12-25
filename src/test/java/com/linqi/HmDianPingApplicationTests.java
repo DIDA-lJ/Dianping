efcf26890feba5d13ad462772a69993a5a41472a
linqi <wkw4399xs@gmail.com>
2023-12-25 20:32:06 +0800

    feat:全局 ID 生成器

--- src/test/java/com/linqi/HmDianPingApplicationTests.java
+++ src/test/java/com/linqi/HmDianPingApplicationTests.java
@@ -1,17 +1,46 @@
 package com.linqi;
 
 import com.linqi.service.impl.ShopServiceImpl;
+import com.linqi.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private final ExecutorService es = Executors.newFixedThreadPool(500);
+    @Test
+    void testRedisIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () ->{
+            for(int i = 0; i < 100; i++){
+                long id =redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for(int i = 0; i < 300; i++){
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin) + " ms");
+    }
 
     @Test
     void testSaveShop(){
