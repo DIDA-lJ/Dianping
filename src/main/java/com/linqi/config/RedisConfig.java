8ae6ab69768257756504f476a59ffad807a67290
linqi <wkw4399xs@gmail.com>
2023-12-29 00:36:14 +0800

    feat: 添加 Redisson 配置

---
+++ src/main/java/com/linqi/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.linqi.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author linqi
+ * @version 1.0.0
+ * @description
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+
+}
