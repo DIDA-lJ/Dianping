50b872fca7abd7c06243805d75eb593e4c188440
linqi <wkw4399xs@gmail.com>
2023-12-28 14:39:05 +0800

    feat: Redis 分布式锁实现

--- src/main/java/com/linqi/utils/SimpleRedisLock.java
+++ src/main/java/com/linqi/utils/SimpleRedisLock.java
@@ -1,6 +1,10 @@
 package com.linqi.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * @author linqi
@@ -18,16 +22,27 @@ public class SimpleRedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        long threadId = Thread.currentThread().getId();
-        return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, String.valueOf(threadId)));
+        String threadId = ID_PREFIX +Thread.currentThread().getId();
+        Boolean success = stringRedisTemplate.opsForValue()
+                .setIfAbsent(KEY_PREFIX + name, threadId,timeoutSec, TimeUnit.SECONDS);
+        return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
+        // 获取线程 id
+        String threadId = ID_PREFIX +Thread.currentThread().getId();
+
+        // 获取锁中的标示
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+
         // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        if (threadId.equals(id)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
