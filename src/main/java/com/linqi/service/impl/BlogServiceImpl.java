a9176fab244080e7e9910024cd6c039bc64ca78b
linqi <wkw4399xs@gmail.com>
2024-01-05 17:49:13 +0800

    feat: 代码恢复,笔记查看功能恢复

--- src/main/java/com/linqi/service/impl/BlogServiceImpl.java
+++ src/main/java/com/linqi/service/impl/BlogServiceImpl.java
@@ -1,52 +1,55 @@
 package com.linqi.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.linqi.constants.SystemConstants;
 import com.linqi.dto.Result;
-import com.linqi.dto.ScrollResult;
-import com.linqi.dto.UserDTO;
 import com.linqi.entity.Blog;
-import com.linqi.entity.Follow;
 import com.linqi.entity.User;
 import com.linqi.mapper.BlogMapper;
 import com.linqi.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.linqi.service.IFollowService;
 import com.linqi.service.IUserService;
-import com.linqi.utils.UserHolder;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.linqi.constants.RedisConstants.BLOG_LIKED_KEY;
-import static com.linqi.constants.RedisConstants.FEED_KEY;
 
 /**
  * <p>
  * 服务实现类
  * </p>
+ * @author linqi
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
-
+    @Resource
+    private IUserService userService;
     @Override
     public Result queryHotBlog(Integer current) {
-        return null;
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog ->{
+            queryBlogUser(blog);
+
+        });
+        return Result.ok(records);
     }
 
     @Override
     public Result queryBlogById(Long id) {
-        return null;
+        //1.查询 blog
+        Blog blog = getById(id);
+        if(blog == null){
+            return Result.fail("笔记不存在");
+        }
+        queryBlogUser(blog);
+        return Result.ok(blog);
     }
 
     @Override
@@ -67,5 +70,12 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     @Override
     public Result queryBlogOfFollow(Long max, Integer offset) {
         return null;
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
     }
 }
