18324cd21230e363c153df23edf05dafdb5ae8b9
LJ <97254796+DIDA-lJ@users.noreply.github.com>
2024-01-06 17:34:20 +0800

    Master (#24)
    * feat: 代码恢复,笔记查看功能恢复
    
    * feat: 文章点赞功能实现，空指针 bug 修复
    
    * feat: 功能补充
    
    * feat: 点赞数 - 1 修改
    
    * feat: pom.xml 文件 Redisson 版本配置重置
    
    * feat: override 重载方法添加
    
    * feat: zset 方法调整
    
    * feat: 查询是否点赞补充用户登录校验功能，防止空指针异常
    
    * feat: 点赞排行榜功能优化
    
    * feat: 关注功能实现
    
    * feat: UserController 根据 id 查询用户功能实现
    
    * feat: BlogController 根据用户id查询用户博客
    
    * feat: BlogController 用户关注写缓存实现
    
    ---------
    
    Signed-off-by: LJ <97254796+DIDA-lJ@users.noreply.github.com>

--- src/main/java/com/linqi/constants/RedisConstants.java
+++ src/main/java/com/linqi/constants/RedisConstants.java
@@ -22,6 +22,7 @@ public class RedisConstants {
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
+    public static final String FOLLOW_KEY ="follows:";
     public static final String FEED_KEY = "feed:";
     public static final String SHOP_GEO_KEY = "shop:geo:";
     public static final String USER_SIGN_KEY = "sign:";
--- src/main/java/com/linqi/service/impl/FollowServiceImpl.java
+++ src/main/java/com/linqi/service/impl/FollowServiceImpl.java
@@ -6,8 +6,14 @@ import com.linqi.entity.Follow;
 import com.linqi.mapper.FollowMapper;
 import com.linqi.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.linqi.service.IUserService;
 import com.linqi.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.linqi.constants.RedisConstants.FOLLOW_KEY;
 
 /**
  * <p>
@@ -19,10 +25,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService userService;
     @Override
     public Result follow(Long followUserId, Boolean isFollow) {
         // 1.获取登录用户
         Long userId = UserHolder.getUser().getId();
+        String key = FOLLOW_KEY + userId;
 
         // 2.判断关注还是取关
         String flag = "关注失败!";
@@ -31,13 +42,21 @@ public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> impleme
             Follow follow = new Follow();
             follow.setUserId(userId);
             follow.setFollowUserId(followUserId);
-            save(follow);
-            flag = "关注成功!";
+            boolean isSuccess = save(follow);
+            if (isSuccess) {
+                // 把关注用户的id，放入redis的set集合 sadd userId followerUserId
+                stringRedisTemplate.opsForSet().add(key, followUserId.toString());
+                flag = "关注成功!";
+            }
         }else{
-            // 4.取关，删除数据
-            remove(new QueryWrapper<Follow>()
-                    .eq("user_id",userId).eq("follow_user_id",followUserId));
-            flag = "取关成功!";
+            // 4.取关，删除 delete from tb_follow where user_id = ? and follow_user_id = ?
+            boolean isSuccess = remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_user_id", followUserId));
+            if (isSuccess) {
+                // 把关注用户的id从Redis集合中移除
+                stringRedisTemplate.opsForSet().remove(key, followUserId.toString());
+                flag = "取关成功!";
+            }
         }
         return Result.ok(flag);
     }
