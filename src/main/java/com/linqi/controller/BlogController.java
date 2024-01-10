fc9ab63e6fbc798592743443d3e6ff48ba4755c0
linqi <wkw4399xs@gmail.com>
2024-01-10 16:42:32 +0800

    feat: 笔记滚动分析查询解决

--- src/main/java/com/linqi/controller/BlogController.java
+++ src/main/java/com/linqi/controller/BlogController.java
@@ -73,5 +73,10 @@ public class BlogController {
         return Result.ok(records);
     }
 
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow(
+            @RequestParam("lastId") Long max, @RequestParam(value = "offset", defaultValue = "0") Integer offset){
+        return blogService.queryBlogOfFollow(max, offset);
+    }
 
 }
