9270ef581bedbb9c38c04d682fbf8d4bd2f1cc06
linqi <wkw4399xs@gmail.com>
2024-01-10 16:41:17 +0800

    feat: VoucherOrderController 代码调整

--- src/main/java/com/linqi/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/linqi/service/impl/VoucherOrderServiceImpl.java
@@ -152,8 +152,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
             // 6.扣减库存
             boolean success = seckillVoucherService.update()
-                    .setSql("stock = stock - 1") // set stock = stock - 1
-                    .eq("voucher_id", voucherId).gt("stock", 0) // where id = ? and stock > 0
+                    // set stock = stock - 1
+                    .setSql("stock = stock - 1")
+                    // where id = ? and stock > 0
+                    .eq("voucher_id", voucherId).gt("stock", 0)
                     .update();
             if (!success) {
                 // 扣减失败
