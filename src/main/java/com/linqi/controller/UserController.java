6da7d5bb6ca20e8263cd5f27370c1b98e9c525a2
linqi <wkw4399xs@gmail.com>
2024-01-12 22:49:11 +0800

    feat: 用户签到功能实现

--- src/main/java/com/linqi/controller/UserController.java
+++ src/main/java/com/linqi/controller/UserController.java
@@ -94,4 +94,9 @@ public class UserController {
         // 返回
         return Result.ok(userDTO);
     }
+
+    @PostMapping("/sign")
+    public Result sign(){
+        return userService.sign();
+    }
 }
